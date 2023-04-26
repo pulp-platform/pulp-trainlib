03f8bde9108f2a89ec692ffda18921adb00f1d94
Dequino <alberto.dequino@unibo.it>
2023-04-26 11:38:09 +0200

    Deleted BUILD folders

--- tests/test_conv2d/BUILD/PULP/GCC_RISCV/conv2d_fp32/kernel/irq_asm.d
+++
@@ -1,8 +0,0 @@
-/home/alberto/pulp-trainlib/tests/test_conv2d/BUILD/PULP/GCC_RISCV//conv2d_fp32/kernel/irq_asm.o: \
- /home/alberto/pulp-sdk/rtos/pulpos/common/kernel/irq_asm.S \
- /home/alberto/pulp-sdk/rtos/pulpos/pulp/include/pos/chips/pulp/config.h \
- /home/alberto/pulp-sdk/rtos/pulpos/pulp_archi/include/archi/pulp_defs.h
-
-/home/alberto/pulp-sdk/rtos/pulpos/pulp/include/pos/chips/pulp/config.h:
-
-/home/alberto/pulp-sdk/rtos/pulpos/pulp_archi/include/archi/pulp_defs.h:
--- tests/test_linear/BUILD/PULP/GCC_RISCV/linear_test/kernel/irq_asm.d
+++
@@ -1,8 +0,0 @@
-/home/alberto/pulp-trainlib/tests/test_linear/BUILD/PULP/GCC_RISCV//linear_test/kernel/irq_asm.o: \
- /home/alberto/pulp-sdk/rtos/pulpos/common/kernel/irq_asm.S \
- /home/alberto/pulp-sdk/rtos/pulpos/pulp/include/pos/chips/pulp/config.h \
- /home/alberto/pulp-sdk/rtos/pulpos/pulp_archi/include/archi/pulp_defs.h
-
-/home/alberto/pulp-sdk/rtos/pulpos/pulp/include/pos/chips/pulp/config.h:
-
-/home/alberto/pulp-sdk/rtos/pulpos/pulp_archi/include/archi/pulp_defs.h:
