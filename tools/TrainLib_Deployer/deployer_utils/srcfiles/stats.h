77b47786147f6fed9c83dfd61257f6f3a36ee79a
Davide Nadalini <d.nadalini@unibo.it>
2024-02-27 15:35:10 +0100

    Simplify performance analisys for tutorial

--- tools/TrainLib_Deployer/deployer_utils/srcfiles/stats.h
+++ tools/TrainLib_Deployer/deployer_utils/srcfiles/stats.h
@@ -46,15 +46,13 @@
 #define INIT_STATS()  
     unsigned long _cycles = 0; \
     unsigned long _instr = 0; \
-    unsigned long _active = 0; \
-    unsigned long _ldext = 0; \
     unsigned long _tcdmcont = 0; \
     unsigned long _ldstall = 0; \
     unsigned long _imiss = 0; \
     int id = 0;
 
 #define PRE_START_STATS()  \
-      pi_perf_conf((1<<PI_PERF_CYCLES) | (1<<PI_PERF_INSTR) | (1<<PI_PERF_ACTIVE_CYCLES) | (1<<PI_PERF_LD_EXT) | (1<<PI_PERF_TCDM_CONT) | (1<<PI_PERF_LD_STALL) | (1<<PI_PERF_IMISS) ); 
+      pi_perf_conf((1<<PI_PERF_CYCLES) | (1<<PI_PERF_INSTR) | (1<<PI_PERF_TCDM_CONT) | (1<<PI_PERF_LD_STALL) | (1<<PI_PERF_IMISS) ); 
 
 
 #define START_STATS()  \
@@ -66,20 +64,16 @@
    pi_perf_stop(); \
       _cycles   = pi_perf_read (PI_PERF_CYCLES); \
       _instr    = pi_perf_read (PI_PERF_INSTR); \
-    	_active   = pi_perf_read (PI_PERF_ACTIVE_CYCLES); \
-      _ldext    = pi_perf_read (PI_PERF_LD_EXT); \
     	_tcdmcont = pi_perf_read (PI_PERF_TCDM_CONT); \
     	_ldstall  = pi_perf_read (PI_PERF_LD_STALL); \
       _imiss    = pi_perf_read (PI_PERF_IMISS); \
     id = pi_core_id(); \
     printf("\n"); \
-    printf("[%d] cycles = %lu\n", id, _cycles); \
-    printf("[%d] instr = %lu\n", id, _instr); \
-    printf("[%d] active cycles = %lu\n", id, _active); \
-    printf("[%d] ext load = %lu\n", id, _ldext); \
-    printf("[%d] TCDM cont = %lu\n", id, _tcdmcont); \
-    printf("[%d] ld stall = %lu\n", id, _ldstall); \
-    printf("[%d] imiss = %lu\n", id, _imiss); 
+    printf("[%d] elapsed clock cycles = %lu\n", id, _cycles); \
+    printf("[%d] number of instructions = %lu\n", id, _instr); \
+    printf("[%d] TCDM contentions = %lu\n", id, _tcdmcont); \
+    printf("[%d] load stalls = %lu\n", id, _ldstall); \
+    printf("[%d] icache miss (clk cycles count) = %lu\n", id, _imiss); 
 
 #else // STATS
 
