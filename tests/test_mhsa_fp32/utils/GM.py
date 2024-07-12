Fixed backward step for softmax. Fixed backward step for mhsa. Replaced broken-backprop softmax function for PyTorch mhsa implementation. Adjusted transpose operations in GM.py. Adjusted softmax buffer and temp required dimensions. Other fixes (formatting to PEP 8 standards) [Not yet extensively tested]

tests/test_mhsa_fp32/utils/GM.py
@@ -34,19 +34,14 @@ class MyNet(nn.Module):
         return self.mhsa(x=x, tgt_len=tgt_len)
 
 
-def hook_fn1(m, i, o):
+def hook_fn1(_, __, o):
     # Hook to write output gradients
-    cont = 0
-    input_grad = []
-    weight_grad = []
-    output_grad = []
-
     f = open("mhsa-grads.h", "w")
 
     print("------------Output Grad------------")
     for grad in o:
         try:
-            output_grad = grad
+            output_grad = torch.transpose(grad, 0, 1)
             f.write('#define G_OUTPUT_SIZE '+str(output_grad.numel())+'\n')
             print(output_grad)
 
@@ -60,13 +55,9 @@ def hook_fn1(m, i, o):
     f.close()
 
 
-def hook_fn2(m, i, o):
+def hook_fn2(_, __, o):
     # Hook for writing output to file
     cont = 0
-    input_grad = []
-    weight_grad = []
-    output_grad = []
-
     f = open("mhsa-output.h", "w")
 
     print("------------Output------------")
@@ -227,18 +218,22 @@ if __name__ == '__main__':
     net.zero_grad()
     loss.backward()
 
-    input_wgt_grad = torch.transpose(net.mhsa.proj_in.weight.grad, 0, 1)
-    output_wgt_grad = torch.transpose(net.mhsa.proj_out.weight.grad, 0, 1)
-    input_grad = inp.grad
+    input_wgt_grad = net.mhsa.proj_in.weight.grad
+    output_wgt_grad = net.mhsa.proj_out.weight.grad
+    input_grad = inp.grad.transpose(1, 2)
 
     # Write gradients to file
     f = open("mhsa-grads.h", 'a')
+
     f.write('#define G_INPUT_WGT_SIZE '+str(input_wgt_grad.numel())+'\n')
     f.write("PI_L2 float INPUT_WGT_GRAD[G_INPUT_WGT_SIZE] = {"+dump.tensor_to_string(input_wgt_grad)+"};\n")
+
     f.write('#define G_OUTPUT_WGT_SIZE '+str(output_wgt_grad.numel())+'\n')
     f.write("PI_L2 float OUTPUT_WGT_GRAD[G_OUTPUT_WGT_SIZE] = {"+dump.tensor_to_string(output_wgt_grad)+"};\n")
+
     f.write("#define G_IN_SIZE "+str(input_grad.numel()) + '\n')
     f.write("PI_L2 float INPUT_GRAD[G_IN_SIZE] = {"+dump.tensor_to_string(input_grad) + "};\n")
+
     f.close()
 
     # Write attention scores to file

tests/test_mhsa_fp32/utils/mhsa.py
@@ -1,15 +1,7 @@
 import numpy as np
-from torch import float32
-from torch import nn
-from torch import Tensor 
-from torch import cuda
 import torch
-from torch.nn import functional as F
+from torch import nn
 
-#define GIST_A  12102203.17133801f
-#define GIST_B  1064986823.010288f
-#define GIST_C  8388608
-#define GIST_D  2139095040 
 
 def fastexp_gist(x):
     with torch.no_grad():
@@ -22,6 +14,7 @@ def fastexp_gist(x):
         result = torch.from_numpy(n)
     
     return result
+
 
 def q_rsqrt(x):
     with torch.no_grad():
@@ -38,10 +31,9 @@ def q_rsqrt(x):
     return result
 
 
-
 def own_softmax_fastexp(x):
     maxes = torch.max(x, -1, keepdim=True)[0]
-    #maxes = torch.swapaxes(maxes, -2, -1) 
+    # maxes = torch.swapaxes(maxes, -2, -1)
     x_exp = fastexp_gist((x-maxes))
     x_exp_sum = torch.sum(x_exp, -1, keepdim=True)
 
@@ -50,11 +42,12 @@ def own_softmax_fastexp(x):
 
 def own_softmax(x):
     maxes = torch.max(x, -1, keepdim=True)[0]
-    #maxes = torch.swapaxes(maxes, -2, -1) 
+    # maxes = torch.swapaxes(maxes, -2, -1)
     x_exp = torch.exp((x-maxes))
     x_exp_sum = torch.sum(x_exp, -1, keepdim=True)
 
     return x_exp/x_exp_sum
+
 
 def threshold(x):
     log2 = 0.6931471805599453
@@ -62,9 +55,11 @@ def threshold(x):
     log2_3 = 0.3330246519889294
     log2_4 = 0.2308350985830834
     log2_5 = 0.1600026977571413
-    x[x < 3.14 ] = (1 - log2 * x + 0.5 * np.power(x, 2) * log2_2 - 0.16 * np.power(x, 3) * log2_3 + 0.0416 * np.power(x, 4) * log2_4 - 0.008 * np.power(x, 5) * log2_5)[x < 3.14]
+    x[x < 3.14] = (1 - log2 * x + 0.5 * np.power(x, 2) * log2_2 - 0.16 * np.power(x, 3) * log2_3 + 0.0416 *
+                   np.power(x, 4) * log2_4 - 0.008 * np.power(x, 5) * log2_5)[x < 3.14]
     x[x >= 3.14] = 0
     return x
+
 
 def own_partial_softmax_simple(x):
     n_heads = x.shape[-3]
@@ -74,10 +69,10 @@ def own_partial_softmax_simple(x):
     print("Softmax input:")
     print(x)
     
-    lines_max = np.max(x_copy, axis = -1)
+    lines_max = np.max(x_copy, axis=-1)
     diff = np.repeat(lines_max, seq_length).reshape(n_heads, seq_length, seq_length) - x_copy
     
-    exp_sum = np.sum(1 / 2**diff, axis = -1)
+    exp_sum = np.sum(1 / 2**diff, axis=-1)
     exp_sum_inverse = 1 / exp_sum
     
     return torch.from_numpy(np.repeat(exp_sum_inverse, seq_length).reshape(n_heads, seq_length, seq_length) / 2**diff)
@@ -91,14 +86,14 @@ def own_partial_softmax_simple_approximate(x):
     print("Softmax input:")
     print(x)
     
-    lines_max = np.max(x_copy, axis = -1)
+    lines_max = np.max(x_copy, axis=-1)
     diff = np.repeat(lines_max, seq_length).reshape(n_heads, seq_length, seq_length) - x_copy
     
-    exp_sum = np.sum(threshold(diff.copy()), axis = -1)
+    exp_sum = np.sum(threshold(diff.copy()), axis=-1)
     exp_sum_inverse = 1 / exp_sum
     
-    return torch.from_numpy(np.repeat(exp_sum_inverse, seq_length).reshape(n_heads, seq_length, seq_length) * threshold(diff.copy()))
-
+    return torch.from_numpy(np.repeat(exp_sum_inverse, seq_length).reshape(n_heads, seq_length, seq_length) *
+                            threshold(diff.copy()))
 
 
 class MultiHeadedSelfAttention(nn.Module):
@@ -111,31 +106,52 @@ class MultiHeadedSelfAttention(nn.Module):
         self.att_dim = att_dim
         self.n_heads = num_heads
         self.head_dim = att_dim // num_heads
-        #self.scaling = (self.head_dim) ** -0.5
+        # self.scaling = (self.head_dim) ** -0.5
         self.scaling = q_rsqrt(self.head_dim)
-        self.scores = None # for visualization
-        #self.softmax = own_softmax
-        #self.softmax = own_partial_softmax_simple
-        self.softmax = own_softmax_fastexp
+        self.scores = None  # for visualization
+        self.softmax = own_softmax
+        # self.softmax = own_partial_softmax_simple
+        # self.softmax = own_softmax_fastexp
 
     def forward(self, x, tgt_len):
-        q, k, v = self.proj_in(x).chunk(3, dim=-1)
+        # OP A & SPLIT
+        # OP 1
+        qkv = self.proj_in(x)
+
+        q = qkv[..., :int(qkv.shape[-1] / 3)]
+        k = qkv[..., int(qkv.shape[-1] / 3):2 * int(qkv.shape[-1] / 3)]
+        v = qkv[..., 2 * int(qkv.shape[-1] / 3):]
+
+        # OP RESHAPE 1
+        # OP 2
         q = q.contiguous().view(tgt_len, self.n_heads, self.head_dim).transpose(0, 1)
         k = k.contiguous().view(tgt_len, self.n_heads, self.head_dim).transpose(0, 1)
         v = v.contiguous().view(tgt_len, self.n_heads, self.head_dim).transpose(0, 1)
 
+        # OP B
+        # OP 3
         scores = torch.bmm(q, k.transpose(1, 2))
 
         assert list(scores.size()) == [self.n_heads, tgt_len, tgt_len]
 
+        # OP C
+        # OP 4
         scores = scores * self.scaling
-        
+
+        # OP D
+        # OP 5
         scores = self.softmax(scores)
 
+        # OP E
+        # OP 6
         scores = torch.bmm(scores, v)
         assert list(scores.size()) == [self.n_heads, tgt_len, self.head_dim]
 
+        # F x L output shape?
         scores = scores.transpose(0, 1).contiguous().view(tgt_len, self.att_dim)
         self.scores = scores
+
+        # OP F
+        # OP 7
         h = self.proj_out(scores)
         return h