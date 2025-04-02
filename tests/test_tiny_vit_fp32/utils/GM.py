a60426b8bab5968d1e5cc07d5588e44d251b157d
diaco <diaconuccalin@gmail.com>
2025-04-02 19:36:19 +0200

    Added onnx parser skeleton to Tiny ViT fp32 test

--- tests/test_tiny_vit_fp32/utils/GM.py
+++ tests/test_tiny_vit_fp32/utils/GM.py
@@ -1,5 +1,8 @@
 import argparse
 
+import onnx
+import torch
+
 from file_writers import model_components_writer
 from model.TinyViT import TinyViT
 from model_configs import MODEL_CONFIGS
@@ -18,13 +21,19 @@ def arg_parse():
     return parser.parse_args()
 
 
+def onnx_parser(onnx_model):
+    return None
+
+
 def main():
+    # Set root directory
     root_dir = "."
 
+    # Parse arguments
     args = arg_parse()
 
+    # Create model
     cfg = MODEL_CONFIGS[args.config]
-
     model = TinyViT(
         img_size=cfg["IN_IMG_SIZE"],
         in_chans=cfg["IN_CHANS"],
@@ -41,6 +50,22 @@ def main():
         local_conv_size=cfg["LOCAL_CONV_SIZE"],
     )
 
+    # Model to ONNX
+    sample_input = torch.randn(1, cfg["IN_CHANS"], cfg["IN_IMG_SIZE"], cfg["IN_IMG_SIZE"])
+    torch.onnx.export(
+        model,
+        sample_input,
+        "TinyViT.onnx",
+        verbose=False,
+        opset_version=20,
+        training=torch.onnx.TrainingMode.EVAL,
+    )
+    onnx_model = onnx.load("TinyViT.onnx")
+
+    # Parse onnx
+    onnx_parser(onnx_model)
+
+    # Write model components
     model_components_writer(file_root_dir=root_dir)
 
 
